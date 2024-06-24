0f452196479ffe16ad1d538b96a126fcc3a2018b
zephyr zhao <2981047480@qq.com>
2024-06-24 18:33:36 +0800

    新加了demo模块，存放了FileServer相关的demo

--- demo/demo.go
+++ demo/demo.go
@@ -4,5 +4,16 @@ import "net/http"
 
 func Demo() {
 	fs := http.FileServer(http.Dir("/Users/zhaozephyr"))
+	/*
+		fs对象如下：
+		type fileHandler struct {
+		root FileSystem
+		}
+
+		其中FileSystem为一个实现了Open方法的接口
+		type FileSystem interface {
+		Open(name string) (File, error)
+		}
+	*/
 	http.Handle("/static/", http.StripPrefix("/static", fs))
 }
